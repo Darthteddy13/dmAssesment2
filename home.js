e88abf0c1052ab8733ed6f5fe6e616f46bed64bc
Kevin Taylor <kevintay96@gmail.com>
2022-06-25 11:29:06 -0600

    finished home.js

--- home.js
+++ home.js
@@ -23,7 +23,7 @@
 
 //CODE HERE
 
-
+const greetUser = username => `welcome back ${username}`;
 
 
 
@@ -50,7 +50,19 @@
 const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
 //CODE HERE
+function canWeDeliver(zipCode)
+{
+    if (deliveryAreaZipCodes.includes(zipCode))
+    {
+        return `You're in our delivery zone!`;
+    }
+    else
+    {
+        return `Sorry, we can't deliver to that address`;
+    }
+}
 
+// console.log(canWeDeliver(85205));
 
 
 /* 
@@ -72,6 +84,20 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
 // CODE HERE
 
+const canWeDeliverTwo = zipCode => 
+{
+    for(let i = 0; i < deliveryAreaZipCodes.length; i++)
+    {
+        if(zipCode === deliveryAreaZipCodes[i])
+            {
+                return `You're in our delivery zone!`;
+            }
+    }
+    return `Sorry, we can't deliver to that address`;
+}
+
+console.log(canWeDeliverTwo(85205));
+console.log(canWeDeliverTwo(84606));
 
 //////////////////PROBLEM 3////////////////////
 /* 
@@ -108,7 +134,7 @@ const deals = [
 
 //CODE HERE
 
-
+deals[0].title = deals[0].title.replace(`15%`, `10%`);
 
 /*
     The restaurant is going to continue its
@@ -124,3 +150,7 @@ const deals = [
 */
 
 //CODE HERE
+
+deals[1].desc = deals[1].desc.trim();
+
+// console.log(deals);
